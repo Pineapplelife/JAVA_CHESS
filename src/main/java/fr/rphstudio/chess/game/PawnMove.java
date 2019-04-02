4aa34c4c7326537c7afac9123bfffbe23ba33503
Unknown <mgueribout@gmail.com>
2019-04-03 00:31:30 +0200

    Story 4 : PawnMove

--- src/main/java/fr/rphstudio/chess/game/PawnMove.java
+++ src/main/java/fr/rphstudio/chess/game/PawnMove.java
@@ -1,7 +1,6 @@
 package fr.rphstudio.chess.game;
 
 import fr.rphstudio.chess.interf.IChess;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +10,26 @@ public class PawnMove implements IMove {
         List<IChess.ChessPosition> chessPositionList;
 
         chessPositionList = new ArrayList<IChess.ChessPosition>();
+        Piece myPiece = board.getPiece(position);
 
-        chessPositionList.add(new IChess.ChessPosition(position.x, position.y-3));
-        chessPositionList.add(new IChess.ChessPosition(position.x, position.y+1));
-
+        if(!myPiece.hasMovedYet()) {
+            if (myPiece.getPieceColor() == IChess.ChessColor.CLR_BLACK) {
+                chessPositionList.add(new IChess.ChessPosition(position.x, position.y + 1));
+                chessPositionList.add(new IChess.ChessPosition(position.x, position.y + 2));
+            }
+            else {
+                chessPositionList.add(new IChess.ChessPosition(position.x, position.y - 1));
+                chessPositionList.add(new IChess.ChessPosition(position.x, position.y - 2));
+            }
+        }
+        else{
+            if(myPiece.getPieceColor() == IChess.ChessColor.CLR_BLACK){
+                chessPositionList.add(new IChess.ChessPosition(position.x, position.y + 1));
+            }
+            else{
+                chessPositionList.add(new IChess.ChessPosition(position.x, position.y - 1));
+            }
+        }
         return chessPositionList;
     }
 }
