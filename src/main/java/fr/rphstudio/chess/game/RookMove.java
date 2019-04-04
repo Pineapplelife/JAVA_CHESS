0b6f48b6aa29e697ab8a01d7d38ea596044d130a
Unknown <maximejoullin@gmail.com>
2019-04-04 10:19:49 +0200

    FIX 2

--- src/main/java/fr/rphstudio/chess/game/RookMove.java
+++ src/main/java/fr/rphstudio/chess/game/RookMove.java
@@ -1,4 +1,57 @@
 package fr.rphstudio.chess.game;
 
-public class RookMove {
+import fr.rphstudio.chess.interf.IChess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RookMove implements IMove {
+    @Override
+    public List<IChess.ChessPosition> getPossibleMoves(IChess.ChessPosition position, ChessBoard board) {
+        List<IChess.ChessPosition> chessPositionList = new ArrayList<>();
+
+        Piece myPiece = board.getPiece(position);
+        IChess.ChessPosition posTemp;
+        Piece pieceTemp;
+
+
+        for (int test = 0; test < 4; test++) {
+            int x = 0;
+            int y = 0;
+
+            switch(test){
+                case 0 :
+                    y = 1;
+                break;
+                case 1 :
+                    x = 1;
+                break;
+                case 2 :
+                    y = -1;
+                break;
+                case 3 :
+                    x = -1;
+                break;
+            }
+
+            for (int i = 1; i <= 7; i++) {
+                posTemp = new IChess.ChessPosition(position.x + (i*x), position.y + (i*y));
+
+                if (posTemp.x < IChess.BOARD_WIDTH && posTemp.x >= 0 && posTemp.y >= 0 && posTemp.y < IChess.BOARD_HEIGHT) {
+                    pieceTemp = board.getPiece(posTemp);
+                    if (pieceTemp != null) {
+                        if (pieceTemp.getPieceColor() != myPiece.getPieceColor()) {
+                            chessPositionList.add(posTemp);
+                        }
+                        break;
+                    } else {
+                        chessPositionList.add(posTemp);
+                    }
+                }
+            }
+        }
+        return chessPositionList;
+    }
 }
+
+
